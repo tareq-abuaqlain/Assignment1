9f28fed41442d8b4542e7b48bafd11fbd181eab8
Tareq Hisham <tareqabuaqlain1@gmail.com>
2023-01-22 12:18:40 +0200

    abstraction code backend

--- server/controller/filterProductCategory.js
+++ server/controller/filterProductCategory.js
@@ -5,18 +5,19 @@ const filterProductCategory = async (req, res) => {
     const { category } = req.params;
     const { sortName, sortPrice } = req.query;
     const data = await filterProductCategoryQuery(category);
+    const dataArr = (dataArray, sortedDataArr) => [sortedDataArr, { 'Total number of products': dataArray.rows.length }, { 'Total price of products': dataArray.rows.reduce((acc, cur) => acc + cur.price, 0) }];
     if (sortPrice === 'true') {
       const sortedData = data.rows.sort((a, b) => b.price - a.price);
-      res.status(200).json([sortedData, { 'Total number of products': data.rows.length }, { 'Total price of products': data.rows.reduce((acc, cur) => acc + cur.price, 0) }]);
+      res.status(200).json(dataArr(data, sortedData));
     } else if (sortPrice === 'false') {
       const sortedData = data.rows.sort((a, b) => a.price - b.price);
-      res.status(200).json([sortedData, { 'Total number of products': data.rows.length }, { 'Total price of products': data.rows.reduce((acc, cur) => acc + cur.price, 0) }]);
+      res.status(200).json(dataArr(data, sortedData));
     } else if (sortName === 'true') {
       const sortedData = data.rows.sort((a, b) => b.product_name.localeCompare(a.product_name));
-      res.status(200).json([sortedData, { 'Total number of products': data.rows.length }, { 'Total price of products': data.rows.reduce((acc, cur) => acc + cur.price, 0) }]);
+      res.status(200).json(dataArr(data, sortedData));
     } else if (sortName === 'false') {
       const sortedData = data.rows.sort((a, b) => a.product_name.localeCompare(b.product_name));
-      res.status(200).json([sortedData, { 'Total number of products': data.rows.length }, { 'Total price of products': data.rows.reduce((acc, cur) => acc + cur.price, 0) }]);
+      res.status(200).json(dataArr(data, sortedData));
     } else {
       res.status(200).json([data.rows, { 'Total number of products': data.rows.length }, { 'Total price of products': data.rows.reduce((acc, cur) => acc + cur.price, 0) }]);
     }
